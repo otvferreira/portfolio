31e24c7b484c4d91183b1048bb88784d0a7c56eb
Otávio Ferreira <ottvferreira@gmail.com>
2024-06-28 22:09:04 -0300

    FEAT: Header

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        '4FC3F7': '#4FC3F7',
+        'FFFFFF': '#FFFFFF',
+        'C1C1C1': '#C1C1C1',
+        '1A1A1A': '#1A1A1A'
+      },
+      spacing: {
+        '2.875': '11.5px',
+        '6.625': '26.5px',
+      },
+    },
+  },
+  plugins: [],
+}
+
