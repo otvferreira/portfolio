c5b82695d75e67e4830f31411156356a80078fb1
Otávio Ferreira <ottvferreira@gmail.com>
2024-07-25 20:13:45 -0300

    FEAT: Adicionando tecnologias abaixo do body

---
+++ src/app/components/tec/tec.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tec',
+  standalone: true,
+  imports: [],
+  templateUrl: './tec.component.html',
+  styleUrl: './tec.component.css'
+})
+export class TecComponent {
+
+}
