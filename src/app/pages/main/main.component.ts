25f79c474cc6be3906d30598e13f187efde118e3
Otávio Ferreira <ottvferreira@gmail.com>
2024-07-25 20:51:07 -0300

    FEAT: Corrigindo icons

--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -4,6 +4,7 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { BodyComponent } from '../../components/body/body.component';
 import { ExperienceComponent } from '../../components/experience/experience.component';
 import { TecComponent } from '../../components/tec/tec.component';
+import { SobreComponent } from '../../components/sobre/sobre.component';
 
 @Component({
   selector: 'app-main',
@@ -14,6 +15,7 @@ import { TecComponent } from '../../components/tec/tec.component';
     BodyComponent,
     TecComponent,
     ExperienceComponent,
+    SobreComponent
 
   ],
   templateUrl: './main.component.html',
